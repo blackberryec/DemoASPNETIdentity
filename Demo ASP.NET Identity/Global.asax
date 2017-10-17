cfdee3c0fe249ba29efdd4219434f508d78551ea
HAITRAN\trann <trannamhaibp@gmail.com>
2017-10-17 20:11:52 +0700

    Add project files.

---
+++ Demo ASP.NET Identity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Demo_ASP.NET_Identity.MvcApplication" Language="C#" %>
---
+++ Demo ASP.NET Identity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Demo_ASP.NET_Identity.Startup))]
+namespace Demo_ASP.NET_Identity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
